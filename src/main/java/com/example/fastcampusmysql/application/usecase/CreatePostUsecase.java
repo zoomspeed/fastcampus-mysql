39bb8073479403099fd12eb2c0245cf5e615b081
sw.yoon@okestro.com <yoonseonwoo@MacBook-Pro-6.local>
2024-06-03 16:49:17 +0900

    Ch 08. 데이터 정합성 보장을 위한 트랜잭션 이해하기 03. 게시물과 타임라인 정합성 보장

--- src/main/java/com/example/fastcampusmysql/application/usecase/CreatePostUsecase.java
+++ src/main/java/com/example/fastcampusmysql/application/usecase/CreatePostUsecase.java
@@ -7,6 +7,7 @@ import com.example.fastcampusmysql.domain.post.service.PostWriteService;
 import com.example.fastcampusmysql.domain.post.service.TimelineWriteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -17,6 +18,10 @@ public class CreatePostUsecase {
 
     final private TimelineWriteService timelineWriteService;
 
+    //transactional을 여기걸면서, undo 로그에 기록되는 시간이 길어짐
+    //(대용량 시스템의 문제, 팔로워가 몇만이라면 트랜잭션이 너무길어짐) -> 커넥션 풀 고갈이슈
+    //락을잡게되면, 락을잡는시간이 굉장히 길어짐
+    //@Transactional
     public Long exectue(PostCommand postCommand) {
         var postId = postWriteService.create(postCommand);
 
--- src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
+++ src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
@@ -7,6 +7,8 @@ import com.example.fastcampusmysql.domain.member.repository.MemberNicknameHistor
 import com.example.fastcampusmysql.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @RequiredArgsConstructor
 @Service
@@ -15,6 +17,7 @@ public class MemberWriteService {
 
     final private MemberNicknameHistoryRepository memberNicknameHistoryRepository;
 
+    //@Transactional
     public Member create(RegisterMemberCommand command) {
         /*
             GOAL - 회원정보(이메일, 닉네임, 생년월일)를 등록한다.
@@ -25,15 +28,27 @@ public class MemberWriteService {
             memberRepository.save(member)
             memberRepository.save(member)
          */
+        //inner함수 getMember를 호출시 transactional이 제대로 동작하지않음 (프록시패턴)
+        return getMember(command);
+    }
+
+    @Transactional
+    private Member getMember(RegisterMemberCommand command) {
         var member = Member.builder()
                 .nickname(command.nickname())
                 .birthday(command.birthday())
                 .email(command.email())
                 .build();
+
+        //TransactionTemplate
+
         var savedMember = memberRepository.save(member);
+        var zeros = 0 / 0;
+
         saveMemberNicknameHistory(savedMember);
         return savedMember;
     }
+
 
     public void changeNickname(Long memberId, String nickname) {
         /**
